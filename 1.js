cd0ff319edf91e77333dc5dcd3fbff318f07a5a2
EkaterinaAlimpieva <AlimpievaOwO@yandex.ru>
2023-06-06 17:00:25 +0500

    add homework

---
+++ 1.js
@@ -0,0 +1,11 @@
+const temperature_c = parseFloat(prompt('Введите температуру по цельсию:'));
+
+
+alert(c_to_f(temperature_c));
+
+
+function c_to_f(temperature_C) {
+
+    return Number(((temperature_c * 9/5) + 32).toFixed(2));
+
+}
